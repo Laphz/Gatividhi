4d72b9db4ec3d9b23396e99c47b362bc9ce4e2fb
Laphz <aman2005mishra@gmail.com>
2025-02-15 00:06:31 +0530

    Gatividhi 3.o

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCiWVJlKegccmUy3U-wa9OQMhKzj7uKhAM",
-  authDomain: "gatividhi-5884e.firebaseapp.com",
-  projectId: "gatividhi-5884e",
-  storageBucket: "gatividhi-5884e.firebasestorage.app",
-  messagingSenderId: "628523622956",
-  appId: "1:628523622956:web:46d74fc19cf665f7076789",
-  measurementId: "G-YJHEF89FJE",
+  apiKey: process.env.EXPO_PUBLIC_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_IN,
+  appId: process.env.EXPO_APP_ID,
+  measurementId: process.env.EXPO_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
